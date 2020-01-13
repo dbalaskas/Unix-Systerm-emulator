16d61ffc6061c710b54206d8e2c378e554f833dc
meryfanou <59747318+meryfanou@users.noreply.github.com>
2020-01-14 00:17:53 +0200

    Update mds.h

--- include/mds.h
+++ include/mds.h
@@ -1,17 +1,26 @@
 /* FILE: mds.h */
 
-typedef struct {
-	unsigned int nodeid;
-	char *filename;
-	unsigned int size;
-	unsigned int type;
-	unsigned int parent_nodeid;
-	time_t creation_time;
-	time_t access_time;
-	time_t modification_time;
-	Datastream data;
-} MDS;
+#include <time.h>
 
 typedef struct {
-	unsigned int datablocks[DATABLOCK_NUM];
+        unsigned int nodeid;
+//      char *filename;
+        unsigned int size;
+        unsigned int type;
+        unsigned int parent_nodeid;
+        time_t creation_time;
+        time_t access_time;
+        time_t modification_time;
+//      Datastream data;
+} MDS;
+/*
+typedef struct {
+        unsigned int datablocks[DATABLOCK_NUM];
 } Datastream;
+*/
+
+/*
+ *
+ * METADATA of each file is saved like that: filename(maxFileNameSize) + MDS + datablocks(maxDataBlocksNum)
+ *
+ * */
