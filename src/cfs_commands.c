a2b030c4c2d3f41be8d628f42effbca0a341f502
meryfanou <59747318+meryfanou@users.noreply.github.com>
2020-01-16 23:04:57 +0200

    Update cfs_commands.c

--- src/cfs_commands.c
+++ src/cfs_commands.c
@@ -22,84 +22,89 @@
 14. cfs create <OPTIONS> <FILE>. Δημιουργία ενός cfs στο αρχείο <FILE>.
 */
 
-void cfs_workwith(char *filename)
+int cfs_workwith(char *filename,bool create_called)
 {
-	int		fd = 0, ignore = 0, n, sum = 0;
-	time_t		current_time;
-	superBlock	sB;
-	MDS		metadata;
+        int     fd = -1, ignore = 0, n, sum = 0;
 
-	CALL(open(filename, O_RDWR),-1,"Error opening file for cfs: ",2,fd);
+        CALL(open(filename, O_RDWR),-1,"Error opening file for cfs: ",2,fd);
 
-	while(sum < sizeof(superBlock))
-	{
-		CALL(read(fd,(&sB)+sum,sizeof(superBlock)),-1,"Error reading from cfs file: ",3,n);	//read superBlock (first block)
-		sum += n;
-	}
+        if(create_called == false)
+        {
+                CALL(lseek(fd,0,SEEK_SET),-1,"Error moving ptr in cfs file: ",5,ignore);
+                while(sum < sizeof(superBlock))
+                {
+                        CALL(read(fd,(&sB)+sum,sizeof(superBlock)),-1,"Error reading from cfs file: ",2,n);
+                        sum += n;
+                }
+        }
 
-	sB.metadataSize = sB.filenameSize + sizeof(MDS);		//??
-	sB.root = 1;
-	sB.blockCounter = 1;
-	CALL(lseek(fd,0,SEEK_SET),-1,"Error moving ptr in cfs file: ",5,ignore);
-	sum = 0;
-	while(sum < sizeof(superBlock))
-	{
-		CALL(write(fd,(&sB)+sum,sizeof(superBlock)),-1,"Error writing in cfs file: ",3,n);	//edit superBlock
-		sum += n;
-	}
-	CALL(lseek(fd,sB.blockSize,SEEK_SET),-1,"Error moving ptr in cfs file: ",5,ignore);		//go to second block
-
-	CALL(write(fd,"/",2),-1,"Error writing in cfs file: ",3,ignore);				//write root's name
-	CALL(lseek(fd,sB.filenameSize-2,SEEK_CUR),-1,"Error moving ptr in cfs file: ",5,ignore);	//leave filenameSize space free
-
-	metadata.nodeid = 1;
-	metadata.size = sB.maxFileSize;
-	metadata.type = Directory;
-	metadata.parent_nodeid = 1;
-
-	current_time = time(NULL);
-	metadata.creation_time = current_time;
-	metadata.access_time = current_time;
-	metadata.modification_time = current_time;
-
-	sum = 0;
-	while(sum < sizeof(MDS))
-	{
-		CALL(write(fd,(&metadata)+sum,sizeof(MDS)),-1,"Error writing in cfs file: ",3,n);
-		sum += n;
-	}
-	for(int i=0; i<(sB.maxDatablockNum); i++)
-	{
-		CALL(write(fd,"-1",2),-1,"Error writing in cfs file: ",3,ignore);
-//		CALL(write(fd,"0",2),-1,"Error writing in cfs file: ",3,ignore);		//2-dimensional array for datablocknum+offset
-	}
+        return fd;
 }
 
 int cfs_create(char *filename,int bSize,int nameSize,int maxFSize,int maxDirFileNum)
 {
-	int		fd = 0, ignore = 0, n, sum = 0;
-	superBlock	sB;
+        int             fd = 0, ignore = 0, n, sum = 0;
+        time_t          current_time;
+        MDS             metadata;
+        Datastream      data;
 
-	CALL(creat(filename, S_IRWXU | S_IXGRP),-1,"Error creating file for cfs: ",1,fd);
+        CALL(creat(filename, S_IRWXU | S_IXGRP),-1,"Error creating file for cfs: ",1,fd);
 
-	sB.blockSize = (bSize != -1) ? bSize : BLOCK_SIZE;
-	sB.filenameSize = (nameSize != -1) ? nameSize : FILENAME_SIZE;
-	sB.maxFileSize = (maxFSize != -1) ? maxFSize : MAX_FILE_SIZE;
-	sB.maxFilesPerDir = (maxDirFileNum != -1) ? maxDirFileNum : MAX_DIRECTORY_FILE_NUMBER;
-	sB.maxDatablockNum = (sB.maxFileSize) / (sB.blockSize);
-	sB.metadataSize = -1;
-	sB.root = -1;
-	sB.blockCounter = 0;				//assume block-counting is zero-based
-	sB.nextSuperBlock = -1;
-	sB.stackSize = 0;
+        sB.blockSize = (bSize != -1) ? bSize : BLOCK_SIZE;
+        sB.filenameSize = (nameSize != -1) ? nameSize : FILENAME_SIZE;
+        sB.maxFileSize = (maxFSize != -1) ? maxFSize : MAX_FILE_SIZE;
+        sB.maxFilesPerDir = (maxDirFileNum != -1) ? maxDirFileNum : MAX_DIRECTORY_FILE_NUMBER;
+        sB.maxDatablockNum = (sB.maxFileSize) / (sB.blockSize);
+        sB.metadataSize = sB.filenameSize + sizeof(MDS);
+        sB.root = 1;
+        sB.blockCounter = 1;                                                                            //assume block-counting is zero-based
+        sB.nodeidCounter = 1;
+        sB.nextSuperBlock = -1;
+        sB.stackSize = 0;
 
-	while(sum < sizeof(superBlock))
-	{
-		CALL(write(fd,(&sB)+sum,sizeof(superBlock)),-1,"Error writing in cfs file: ",3,n);
-		sum += n;
-	}
-	CALL(close(fd),-1,"Error closing file for cfs: ",4,ignore);
+        while(sum < sizeof(superBlock))
+        {
+                CALL(write(fd,(&sB)+sum,sizeof(superBlock)),-1,"Error writing in cfs file: ",3,n);
+                sum += n;
+        }
 
-	return fd;
+        CALL(lseek(fd,sB.blockSize,SEEK_SET),-1,"Error moving ptr in cfs file: ",5,ignore);             //go to second block
+
+        CALL(write(fd,"/",2),-1,"Error writing in cfs file: ",3,ignore);                                //write root's name
+        CALL(lseek(fd,sB.filenameSize-2,SEEK_CUR),-1,"Error moving ptr in cfs file: ",5,ignore);        //leave filenameSize space free
+
+        metadata.nodeid = 1;
+        metadata.size = 0;
+        metadata.type = Directory;
+        metadata.parent_nodeid = 1;
+
+        current_time = time(NULL);
+        metadata.creation_time = current_time;
+        metadata.access_time = current_time;
+        metadata.modification_time = current_time;
+
+        sum = 0;
+        while(sum < sizeof(MDS))                                                                        //root's metadata
+        {
+                CALL(write(fd,(&metadata)+sum,sizeof(MDS)),-1,"Error writing in cfs file: ",3,n);
+                sum += n;
+        }
+        data.datablocks = (Location*)malloc((sB.maxDatablockNum)*sizeof(Location));                     //root's data
+        for(int i=0; i<(sB.maxDatablockNum); i++)
+        {
+                (data.datablocks[i]).blocknum = 0;
+                (data.datablocks[i]).offset = 0;
+                sum = 0;
+                while(sum < sizeof(Location))
+                {
+                        CALL(write(fd,&(data.datablocks[i])+sum,sizeof(Location)),-1,"Error writing in cfs file: ",3,n);
+                        sum += n;
+                }
+        }
+
+        CALL(close(fd),-1,"Error closing file for cfs: ",4,ignore);
+
+        free(data.datablocks);
+        return fd;
 }
 
