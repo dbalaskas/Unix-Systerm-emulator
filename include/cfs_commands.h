44d68a68e765773e1f3981e590f38a86e8a6a1ca
meryfanou <59747318+meryfanou@users.noreply.github.com>
2020-01-16 11:07:54 +0200

    until workwith

---
+++ include/cfs_commands.h
@@ -0,0 +1,40 @@
+/* FILE: cfs_commands.h */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "mds.h"
+#include "disk.h"
+
+typedef enum {false, true} bool;
+
+#define CALL(call,error,errString,errNum,returnVal)	\
+returnVal = call;					\
+if(returnVal == error)					\
+{							\
+	if(errString != NULL)				\
+		perror(errString);			\
+	exit(errNum);					\
+}
+
+
+/*
+1. cfs workwith <FILE>
+2. cfs mkdir <DIRECTORIES>
+3. cfs touch <OPTIONS> <FILES>
+4. cfs pwd
+5. cfs cd <PATH>
+6. cfs ls <OPTIONS> <FILES>
+7. cfs cp <OPTIONS> <SOURCE> <DESTINATION> | <OPTIONS> <SOURCES> ... <DIRECTORY>
+8. cfs cat <SOURCE FILES> -o <OUTPUT FILE>
+9. cfs ln <SOURCE FILES> <OUTPUT FILE>
+10. cfs mv <OPTIONS> <SOURCE> <DESTINATION> | <OPTIONS> <SOURCES> ... <DIRECTORY>
+11. cfs rm <OPTIONS> <DESTINATIONS>
+12. cfs import <SOURCES> ... <DIRECTORY>
+13. cfs export <SOURCES> ... <DIRECTORY>
+14. cfs create <OPTIONS> <FILE>. Δημιουργία ενός cfs στο αρχείο <FILE>.
+*/
+
+void cfs_workwith(char*);
+int cfs_create(char*,int,int,int,int);
+
---
+++ include/mds.h
@@ -0,0 +1,28 @@
+/* FILE: mds.h */
+
+#include <time.h>
+
+typedef enum {File, Directory, Link} cfs_type;
+
+typedef struct {
+	unsigned int nodeid;
+//	char *filename;
+	unsigned int size;
+	cfs_type type;
+	unsigned int parent_nodeid;
+	time_t creation_time;
+	time_t access_time;
+	time_t modification_time;
+//	Datastream data;
+} MDS;
+/*
+typedef struct {
+	unsigned int datablocks[DATABLOCK_NUM];
+} Datastream;
+*/
+
+/*
+ *
+ * METADATA of each file is saved like that: filename(maxFileNameSize) + MDS + datablocks(maxDataBlocksNum)
+ *
+ * */
